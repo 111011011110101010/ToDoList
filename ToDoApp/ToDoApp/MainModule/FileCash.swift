3f6691b3581a7a925a19cd312ce5f4ac9598c330
Vladislav <vlad_zaharchenko@list.ru>
2024-06-21 21:37:37 +0300

    homeWork1

---
+++ ToDoApp/ToDoApp/MainModule/FileCash.swift
@@ -0,0 +1,8 @@
+//
+//  FileCash.swift
+//  ToDoApp
+//
+//  Created by newuser on 21.06.2024.
+//
+
+import Foundation
---
+++ ToDoApp/ToDoApp/MainModule/ToDoItem.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItem.swift
+//  ToDoApp
+//
+//  Created by newuser on 21.06.2024.
+//
+
+import Foundation
